85af8127a4b01f672a328bd51898df7ee687b7f8
Mario Franca Wijaya <mariofranca1968@gmail.com>
2024-06-04 21:20:12 +0800

    modul3

---
+++ Unscramble/UnscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,20 @@
+package com.example.android.unscramble.ui.game
+
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// List with all the words for the Game
+val allWordsList: List<String> =
+    listOf("hewan", "mobil", "anekdot", "alfabet", "semua", "hebat", "bangkit", "balon", "keranjang", "bangku", "terbaik", "ulang tahun", "buku", "tas kerja", "kamera", "berkemah",
+        "lilin", "kucing", "kembang kol", "obrolan", "anak-anak", "kelas", "klasik", "ruang kelas", "kopi", "berwarna-warni", "kue", "kreatif", "pelayaran", "menari", "siang hari",
+        "dinosaurus", "kenop pintu", "makan malam", "mimpi", "senja", "makan", "gajah", "zamrud", "mengerikan", "listrik", "selesai", "bunga", "mengikuti", "rubah", "bingkai",
+        "bebas", "sering", "corong", "hijau", "gitar", "belanja", "gelas", "hebat", "tertawa kecil", "potong rambut", "setengah", "buatan sendiri", "terjadi", "madu", "bergegas",
+        "seratus", "es", "iglo", "berinvestasi", "mengundang", "ikon", "memperkenalkan", "lelucon", "ceria", "jurnal", "melompat", "bergabung", "kanguru", "papan ketik", "dapur", "koala",
+        "baik hati", "kaleidoskop", "pemandangan", "terlambat", "tertawa", "belajar", "lemon", "surat", "lili", "majalah", "laut", "permen marshmallow", "labirin", "bermeditasi", "melodi",
+        "menit", "monumen", "bulan", "sepeda motor", "gunung", "musik", "utara", "hidung", "malam", "nama", "tidak pernah", "bernegosiasi", "nomor", "berlawanan", "gurita", "oak",
+        "pesanan", "buka", "kutub", "kemas", "lukisan", "orang", "piknik", "bantal", "pizza", "podcast", "presentasi", "anak anjing", "teka-teki", "resep", "melepaskan", "restoran",
+        "berputar", "mundur", "ruangan", "lari", "rahasia", "benih", "kapal", "kemeja", "seharusnya", "kecil", "pesawat luar angkasa", "melihat bintang", "keterampilan", "jalan", "gaya", "matahari terbit", "taksi",
+        "rapi", "pengatur waktu", "bersama", "gigi", "turis", "bepergian", "truk", "di bawah", "berguna", "unicorn", "unik", "meningkatkan", "seragam", "vas", "biola", "pengunjung", "visi",
+        "volume", "pemandangan", "walrus", "berkelana", "dunia", "musim dingin", "baik", "angin puyuh", "sinar-X", "xilofon", "yoga", "yogurt", "yoyo", "kamu", "tahun", "lezat", "zebra",
+        "zigzag", "zoologi", "zona", "semangat")
